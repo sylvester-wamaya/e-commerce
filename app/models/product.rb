8d7dcd91f0e155d11a6e282f74a4b5e687725797
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-04-13 21:22:40 +0300

    Add reltionship to oerdproducts model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :stocks
+  has_many :order_products
 
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [50,50]
