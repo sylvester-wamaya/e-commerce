7fa1948adf1006942202f4dacfc1f19c39f7671e
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-04-14 19:55:40 +0300

    Implemented a line graph

--- app/javascript/controllers/dashboard_controller.js
+++ app/javascript/controllers/dashboard_controller.js
@@ -1,7 +1,58 @@
 import { Controller } from "@hotwired/stimulus"
 
+import { Chart, registerables } from 'chart.js'
+
+Chart.register(...registerables)
+
 // Connects to data-controller="dashboard"
 export default class extends Controller {
-  connect() {
+  initialize() {
+    const data = [10, 20, 30, 50, 60, 90]
+    const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+
+    const ctx = document.getElementById('revenueChart')
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Revenue $',
+          data: data,
+          borderWidth: 3,
+          fill: true
+        }]
+      },
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: 'Revenue Chart'
+          },
+          legend: {
+            display: false
+          },
+        },
+
+        scales: {
+          x: {
+            grid: {
+              display: false
+            }
+          },
+          y: {
+            border: {
+              dash: [5, 5]
+            },
+            grid: {
+              color: "#d4f3ef",
+
+            },
+            beginAtZero: true
+          }
+        }
+      }
+    }
+    )
   }
 }
