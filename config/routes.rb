d394a04c91c5d6fbd168bf90ab73349cca44e591
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-05 23:54:07 +0300

    category routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,9 @@ Rails.application.routes.draw do
      root to: "admin/#index", as: :admin_root
    end
 
+   resources :categories, only: [:show]
+
    get "admin" => "admin#index"
 
-   get "category" => "category#index"
+   
 end
