67e78a25c80dd5ba01422e45bf497f6e45a9c8cf
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-06 00:12:46 +0300

    Added image variant

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,6 @@ class Product < ApplicationRecord
 
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [50,50]
+    attachable.variant :medium, resize_to_limit: [250,250]
 end
 end
