4f697194105d8b554e02663bd7b4278006ce911c
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-12 00:51:38 +0300

    Impelement products stimulus

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -4,7 +4,35 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static values = {size: String, product: Object}
   addToCart() {
-    console.log("product: ", this.productValue)
+    const cart = localStorage.getItem('cart')
+    if(cart) {
+      const cartArray = JSON.parse(cart)
+      const foundIndex = cartArray.findIndex(product => product.id === this.productValue.id && product.size === this.sizeValue)
+      if(foundIndex >=0) {
+        cartArray[foundIndex].quantity = parseInt(cartArray[foundIndex].quantity) + 1
+      }
+      else {
+        cartArray.push({
+          id: this.productValue.id,
+          name: this.productValue.name,
+          price: this.productValue.price,
+          size: this.sizeValue,
+          quantity: 1
+        })
+      }
+      localStorage.setItem('cart', JSON.stringify(cartArray))
+    }
+    else {
+      const cartArray = []
+      cartArray.push({
+        id: this.productValue.id,
+        name: this.productValue.name,
+        price: this.productValue.price,
+        size: this.sizeValue,
+        quantity: 1
+      })
+      localStorage.setItem('cart', JSON.stringify(cartArray))
+    }
   }
   selectSize(e) {
     this.sizeValue = e.target.value
