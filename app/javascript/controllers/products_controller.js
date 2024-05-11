917961a5ee67a393d4c755b3531e69566cfb547e
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-12 00:17:19 +0300

    Implement selecteSize method and add to Cart

--- app/javascript/controllers/products_controller.js
+++ app/javascript/controllers/products_controller.js
@@ -2,6 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="products"
 export default class extends Controller {
-  connect() {
+  static values = {size: String, Product: Object}
+  addToCart() {
+    console.log("product: ", this.ProductValue)
   }
+  selectSize(e) {
+    this.sizeValue = e.target.value
+      const selectedSizeEl = document.getElementById('selected-size')
+      selectedSizeEl.innerText = `Selected Size: ${this.sizeValue}`
+    
+  };
 }
