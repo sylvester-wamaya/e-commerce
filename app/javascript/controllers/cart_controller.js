71fa87b4995d61471a004c97f47641181fc725ad
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-12 03:14:08 +0300

    Implement stripe checkout

--- app/javascript/controllers/cart_controller.js
+++ app/javascript/controllers/cart_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="cart"
 export default class extends Controller {
   initialize() {
-    console.log('Cart controller initialized')
+ 
     const cart = JSON.parse(localStorage.getItem('cart'))
     if (!cart) {
       return
@@ -64,25 +64,22 @@ export default class extends Controller {
       body: JSON.stringify(payload)
     }).then(response => {
       if(response.ok) {
-        window.location.href = body.url
+        response.json().then(body => {
+          window.location.href = body.url
+        }
+        )
+        
       }
       else {
-        const errorEl = document.createElement('div')
+        response.json().then(body => {
+          const errorEl = document.createElement('div')
         errorEl.innerText = `Error checking out. ${body.error}`
         let errorContainer = document.getElementById('errorContainer')
         errorContainer.appendChild(errorEl)
+         
+        })
+        
       }
-    }).then(data => {
-      console.log(data)
-    })
-
-    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0)
-    const stripe = Stripe('pk_test_51J4Kb4D4X1tY5RgX4f3V3V7fJ8V7d2Zr2Qv9z7Z8iW2zvFQ4V0m4yqYVwVv9cLm5KQF5qJ6g1uG4L2w2t0YQ1X8b00J8JwJZv5')
-    stripe.redirectToCheckout({
-      lineItems: cart.map(item => ({price: item.priceId, quantity: item.quantity})),
-      mode: 'payment',
-      successUrl: 'http://localhost:3000/',
-      cancelUrl: 'http://localhost:3000/cart'
     })
   }
 }
